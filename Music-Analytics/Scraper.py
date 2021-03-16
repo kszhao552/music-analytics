74c1492bf7b34da3942d20ca85093cedc65556c2
Kradon <kradonzhao@gmail.com>
2021-03-16 19:57:25 -0400

    compile results into list
    Took all the results that are pulled from the website and put it into a list with the data in a tuple.
    Converted the script to a function to allow for more modularity.
    TODO: Used spotipy to automate creating a playlist of each songs to obtain the track time.

--- Music-Analytics/Scraper.py
+++ Music-Analytics/Scraper.py
@@ -4,22 +4,31 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 from bs4 import BeautifulSoup
 
-for i in range(2006, 2021):
+def getLists():
+    yearEndLists = []
 
-    URL = f'https://www.billboard.com/charts/year-end/{i}/hot-100-songs'
-    page = requests.get(URL)
+    for i in range(2006, 2021):
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+        URL = f'https://www.billboard.com/charts/year-end/{i}/hot-100-songs'
+        page = requests.get(URL)
 
-    results = soup.find_all('article', class_ = "ye-chart-item")
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        results = soup.find_all('article', class_ = "ye-chart-item")
    
+        yearList = []
+        for result in results:
+            rank = result.find('div', class_ = "ye-chart-item__rank")
+            title = result.find('div', class_ = "ye-chart-item__title")
+            artist = result.find('div', class_ = "ye-chart-item__artist")
+        
+            yearList.append((int(rank.text.split('\n')[1]), title.text.split('\n')[1], artist.text.split('\n')[int(len(artist.text.split('\n'))/2)]))
 
-    for result in results:
-        rank = result.find('div', class_ = "ye-chart-item__rank")
-        title = result.find('div', class_ = "ye-chart-item__title")
-        artist = result.find('div', class_ = "ye-chart-item__artist")
+        yearEndLists.append(yearList)
+        return yearEndLists
 
-        print(f'{rank.text.strip()} {title.text.strip()} - {artist.text.strip()}')
+
+pprint.pprint(getLists())
 
 
 
