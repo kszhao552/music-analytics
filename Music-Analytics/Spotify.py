6aebccdf54e91e75eb88696377a695bf22cfd1d5
Kradon <kradonzhao@gmail.com>
2021-03-18 21:15:45 -0400

    initial commit
    Class that will utilize the spotify API.
    Creates and store the credentials and the authorization so it does not need to be done repeatedly.
    getTrack method will allow the search of a song's info from the track name.

---
+++ Music-Analytics/Spotify.py
@@ -0,0 +1,22 @@
+import spotipy
+import spotipy.oauth2 as oauth2
+import json
+
+class Spotify(object):
+    """description of class"""
+
+    def __init__(self):
+        with open('Keys.json', 'r') as f:
+            config = json.load(f)
+ 
+        client = config['client']
+        secret = config['secret']
+
+        credentials = oauth2.SpotifyClientCredentials(client_id = client, client_secret = secret)
+
+        token = credentials.get_access_token()
+        self.sp = spotipy.Spotify(auth = token)
+
+
+    def getTrack(self, track):
+        return self.sp.search(q=f'track: {track}', type = 'track', limit =1)
