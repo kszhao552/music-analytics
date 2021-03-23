02787af3f043338c71d2d73de97132c923a3a289
Kradon <kradonzhao@gmail.com>
2021-03-23 19:56:26 -0400

    add database schema in comments
    Three tables are required, table for the tracks, artist and the connection for track to artist.
    Tables will be created in the database creation function.

--- Music-Analytics/Database.py
+++ Music-Analytics/Database.py
@@ -3,6 +3,21 @@ from mysql.connector import connect, Error
 import Scraper
 from Track import Track
 
+#Track Table Schema:
+#id
+#title
+#chart_year
+#length
+#tempo, dancability, energy, key, loudness, speechiness, acousticness, liveness, valence
+
+#Artist Table Schema:
+#id
+#name
+
+#Song Table Schema:
+#track_id (Foreign key)
+#artist_id (Foreign key)
+
 def establishConnection():
     try:
         db = connect(
@@ -14,5 +29,9 @@ def establishConnection():
     except Error as e:
         print(e)
 
+def createDatabase(db, title):
+    cursor = db.cursor()
+
+    cursor.execute(f"create database IF NOT EXISTS {title}")
 
 establishConnection()
