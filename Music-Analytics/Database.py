3d206f5f0412fe720a1003c7e41c45d3d808c81f
Kradon <kradonzhao@gmail.com>
2021-03-23 21:15:18 -0400

    create tables within database.
    In the createDatabase function, the tables will be created inside the database.
    All three tables have been created.
    TODO: Create insertion statement for each value in the tables.

--- Music-Analytics/Database.py
+++ Music-Analytics/Database.py
@@ -33,5 +33,40 @@ def createDatabase(db, title):
     cursor = db.cursor()
 
     cursor.execute(f"create database IF NOT EXISTS {title}")
+    
+    str = """
+    create TABLE Tracks (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(70), 
+        year YEAR(4), 
+        length DECIMAL(7, 3),
+        dancebility DECIMAL(5, 3),
+        energy DECIMAL(5, 3), 
+        liveness DECIMAL(5, 3),
+        loudness DECIMAL(6, 3),
+        speechiness DECIMAL (5, 3),
+        tempo DECIMAL (7, 3),
+        valence DECIMAL(5, 3)
+    )
+    """
+
+    cursor.execute(str)
+
+    str = """
+    create TABLE Artists (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(70)
+    )
+    """
+    cursor.execute(str)
+
+    str = """
+    create TABLE Songs(
+    FOREIGN KEY(track_id) REFERENCES track(id),
+    FOREIGN KEY(artist_id) REFERENCES artist(id),
+    PRIMARY KEY(track_id, artist_id)
+    )
+    """
+    cursor.execute(str)
 
 establishConnection()
