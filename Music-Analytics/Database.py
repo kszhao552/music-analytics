a3ddaa2d1cd0939c724c98c9d62e2018545a8adf
Kradon <kradonzhao@gmail.com>
2021-03-26 22:59:07 -0400

    add connection to the server with a created database.
    This will make it so that the already database can be connected to and the working scope can be more defined.

--- Music-Analytics/Database.py
+++ Music-Analytics/Database.py
@@ -27,10 +27,25 @@ def establishConnection():
         )
         cursor = db.cursor()
         
+        print("Connection to MySQL Server Successful")
         return db
     except Error as e:
         raise(e)
 
+def restablishConnection(database):
+    try:
+        db = connect(
+            host="localhost",
+            user=input("Enter username: "),
+            password=getpass("Enter password: "),
+            db = database
+        )
+        cursor = db.cursor()
+        
+        print("Connection to MySQL Database Successful")
+        return db
+    except Error as e:
+        raise(e)
 
 def createDatabase(db, title):
     cursor = db.cursor()
@@ -46,16 +61,17 @@ def createTables(db):
     
     CreateTracksQuery = """
     create TABLE IF NOT EXISTS Tracks (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY, 
         title VARCHAR(70), 
         year YEAR(4), 
-        length DECIMAL(7, 3),
-        dancebility DECIMAL(5, 3),
+        length DECIMAL(7, 3), 
+        dancebility DECIMAL(5, 3), 
         energy DECIMAL(5, 3), 
-        liveness DECIMAL(5, 3),
-        loudness DECIMAL(6, 3),
-        speechiness DECIMAL (5, 3),
-        tempo DECIMAL (7, 3),
+        liveness DECIMAL(5, 3), 
+        loudness DECIMAL(6, 3), 
+        speechiness DECIMAL (5, 3), 
+        tempo DECIMAL (7, 3), 
+        music_key INT, 
         valence DECIMAL(5, 3)
     )
     """
@@ -63,15 +79,15 @@ def createTables(db):
 
     CreateArtistsQuery = """
     create TABLE IF NOT EXISTS Artists (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY, 
         name VARCHAR(70)
     )
     """
 
     CreateSongsQuery = """
     create TABLE IF NOT EXISTS Songs(
-    FOREIGN KEY(track_id) REFERENCES track(id),
-    FOREIGN KEY(artist_id) REFERENCES artist(id),
+    FOREIGN KEY(track_id) REFERENCES track(id), 
+    FOREIGN KEY(artist_id) REFERENCES artist(id), 
     PRIMARY KEY(track_id, artist_id)
     )
     """
@@ -93,7 +109,9 @@ def insertTable(track, db):
         return
 
 
-try:
-    print(establishConnection())
-except Error as e:
-    print(f'Error: {e}')
+
+if __name__ == "__main__":
+    try:
+        db = restablishConnection("test")
+    except Error as e:
+        print(f'Error: {e}')
