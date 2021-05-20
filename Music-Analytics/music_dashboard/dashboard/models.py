5ae3907079cdd1801b828911efc9762ac0173233
Kradon <kradonzhao@gmail.com>
2021-05-20 15:15:02 -0500

    change osrt order
    Was ordered based off when they were added before.
    Artists now ordered alphabetically.
    Tracks ordered alphabetically and then by year after that.

--- Music-Analytics/music_dashboard/dashboard/models.py
+++ Music-Analytics/music_dashboard/dashboard/models.py
@@ -4,17 +4,29 @@ from django.db import models
 class Artist(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    class Meta(object):
+        ordering = ['name']
+
 class Track(models.Model):
     track_name = models.CharField(max_length=100)
+    rank = models.PositiveSmallIntegerField(blank=True, null=True)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     spotify_id = models.CharField(max_length=25)
-    length = models.DecimalField(max_digits=7, decimal_places=2)
-    dancibility = models.DecimalField(max_digits=7, decimal_places=2)
-    energy = models.DecimalField(max_digits=7, decimal_places=2)
-    loudness = models.DecimalField(max_digits=7, decimal_places=2)
-    speechiness = models.DecimalField(max_digits=7, decimal_places=2)
-    tempo = models.DecimalField(max_digits=7, decimal_places=2)
-    valence = models.DecimalField(max_digits=7, decimal_places=2)
+    length = models.DecimalField(max_digits=7, decimal_places=3)
+    danceability = models.DecimalField(max_digits=7, decimal_places=3)
+    energy = models.DecimalField(max_digits=7, decimal_places=3)
+    loudness = models.DecimalField(max_digits=7, decimal_places=3)
+    speechiness = models.DecimalField(max_digits=7, decimal_places=3)
+    tempo = models.DecimalField(max_digits=7, decimal_places=3)
+    valence = models.DecimalField(max_digits=7, decimal_places=3)
     music_key = models.PositiveSmallIntegerField(blank=True, null=True)
+    acousticness = models.DecimalField(max_digits=7, decimal_places=3)
+    liveness = models.DecimalField(max_digits=7, decimal_places=3)
     artist = models.ManyToManyField(Artist)
 
+    def __str__(self):
+        return self.track_name
+    class Meta(object):
+        ordering = ['track_name', 'year']
