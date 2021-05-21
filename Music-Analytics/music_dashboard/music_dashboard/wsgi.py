128b73002176c161f659f574a0e9c653d9360f5d
Kradon <kradonzhao@gmail.com>
2021-05-20 19:29:55 -0500

    update files

--- Music-Analytics/music_dashboard/music_dashboard/wsgi.py
+++ Music-Analytics/music_dashboard/music_dashboard/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from . import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_dashboard.settings')
 
