7aa3d54ebe4c8568e665481a5c338e5298107b84
Kradon <kradonzhao@gmail.com>
2021-05-21 13:51:49 -0500

    inital commit

---
+++ Music-Analytics/music_dashboard/dashboard/templatetags/range.py
@@ -0,0 +1,16 @@
+#https://stackoverflow.com/questions/1107737/numeric-for-loop-in-django-templates
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='range')
+def _range(_min, args=None):
+    _max, _step = None, None
+    if args:
+        if not isinstance(args, int):
+            _max, _step = map(int, args.split(','))
+        else:
+            _max = args
+    args = filter(None, (_min, _max, _step))
+    return range(*args)
