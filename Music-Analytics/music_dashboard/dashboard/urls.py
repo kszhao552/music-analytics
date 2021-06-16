246c794602dbbe1620113ec907d0cc50dc0b4c88
Kradon <kradonzhao@gmail.com>
2021-06-15 21:09:52 -0500

    update urls

--- Music-Analytics/music_dashboard/dashboard/urls.py
+++ Music-Analytics/music_dashboard/dashboard/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import TrackListView, TrackYearListView
 
 urlpatterns = [
     path('', views.home, name='dashboard-home'),
+    path('data/', TrackListView.as_view(), name='track-data'),
+    path('data/<int:year>/', TrackYearListView.as_view(), name='track-year')
+
     ]
